5b6b6c5f13c719706f39eacc469999ab48db534a
Adrien <espel.adrien@gmail.com>
2016-04-01 17:12:33 +0200

    update package.json

--- bin/gulp/tasks/dev.js
+++ bin/gulp/tasks/dev.js
@@ -8,12 +8,10 @@ var config = require('../configs/config.js');
 
 // Tasks
 require('./sass');
-require('./browsersync');
 
 gulp.task('dev', function(callback){
 
     callback = callback || function() {};
     gulp.watch([config.dev.sass, config.dev.sassPart], ['sass']);
-    runSequence([ 'browsersync'], callback);
 
 });
